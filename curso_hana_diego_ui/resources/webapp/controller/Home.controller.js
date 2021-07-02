81dd993b1b6d278cf827a011be3100716a49c774
DYEGROS DYEGROS <dyegros@allianceconsultoria.com.br>
2021-07-02 13:28:38 -0300

    initial

---
+++ curso_hana_diego_ui/resources/webapp/controller/Home.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("curso_hana_ui.curso_hana_diego_ui.controller.Home", {
+		onInit: function () {
+
+		}
+	});
+});
