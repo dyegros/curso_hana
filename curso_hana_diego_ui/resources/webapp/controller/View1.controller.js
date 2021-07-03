fe62064856f8164ce574fdfd656bd01156104371
DYEGROS DYEGROS <dyegros@allianceconsultoria.com.br>
2021-07-03 08:58:10 -0300

    First

--- curso_hana_diego_ui/resources/webapp/controller/View1.controller.js
+++ curso_hana_diego_ui/resources/webapp/controller/View1.controller.js
@@ -5,7 +5,11 @@ sap.ui.define([
 
 	return Controller.extend("curso_hana_diego_ui.curso_hana_diego_ui.controller.View1", {
 		onInit: function () {
-
+            var receitasModel = this.getOwnerComponent().getModel("receitasModel");
+            var oTable = this.getView().byId("stReceitas");
+            
+            oTable.setModel(receitasModel);
+            oTable.setInitiallyVisibleFields("MANDT,EMPRESA,FILIAL,ANO,MES,VALOR");
 		}
 	});
 });
